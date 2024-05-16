000cb187f93a805ce771791e7ea65abf749f650f
ruos-sovanra <sovanraruos@gmail.com>
2024-05-16 10:37:39 +0700

    Update cors config

---
+++ src/main/java/com/example/user/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.user.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.rmi.registry.Registry;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
